bf24b691201d73c7bd24260389162c2f4a936e2e
Jhonathan Soares <jhonathanmarolo@hotmail.com>
2018-01-18 14:35:25 -0200

    Add files via upload

---
+++ LICENSE.md/ConfigAcess/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConfigAcess.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigAcess.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            // Obtendo via Key
+            ViewData["SectionConfig1"] = _configuration["Section:Config1"];
+
+            // Obtendo via método GetSection 
+            ViewData["SectionConfig2"] = _configuration.GetSection("Section").GetSection("Config2").GetSection("Subconfig1").Value;
+
+            //Obtendo via médoto GetSection e via Key de forma mixada
+            ViewData["SectionConfig3"] = _configuration.GetSection("Section")["Config2:Subconfig2"];
+
+            return View();
+        }
+    }
+
+}
